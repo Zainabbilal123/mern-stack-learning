4ca87c6a01c8d091a467d5ce65943536abd18586
Your Name <bilalzainab123agmail.com>
2026-06-13 14:32:01 +0500

    did backend

--- week8-deploy-final/backend/server.js
+++ week8-deploy-final/backend/server.js
@@ -21,5 +21,9 @@ if (require.main === module) {
             process.exit(1);
         });
 }
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+});
 
 module.exports = app;
